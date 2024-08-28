b1f747749e52371ce81e37997ddf592eb52d9c2f
Shravya <shravyapandey0@gmail.com>
2024-08-28 19:41:44 +0530

    sixth_commit

---
+++ src/main/java/ParkingLot/Bike.java
@@ -0,0 +1,10 @@
+package ParkingLot;
+
+public class Bike extends Vehicle {
+
+    public Bike(VehicleType type, String vehicleNo, String color) {
+
+        super(VehicleType.BIKE, vehicleNo, color);
+
+    }
+}
---
+++ src/main/java/ParkingLot/Car.java
@@ -0,0 +1,7 @@
+package ParkingLot;
+
+public class Car extends Vehicle {
+    public Car(VehicleType type, String vehicleNo, String color) {
+        super(VehicleType.CAR, vehicleNo, color);
+    }
+}
