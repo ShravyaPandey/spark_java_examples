3a59b0617a2de4edd90b0ec3ee0d6e2515a63189
Shravya <shravyapandey0@gmail.com>
2024-08-30 01:06:26 +0530

    eight_commit

--- src/main/java/functional_programming/Encapsulation.java
+++ src/main/java/functional_programming/Encapsulation.java
@@ -1,2 +1,45 @@
-package functional_programming;public class Encapsulation {
+package functional_programming;
+
+class encapsulate {
+    private int speed;
+
+    public encapsulate(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if(speed > 0)
+            this.speed = speed;
+    }
+
+    public void increaseSpeed(int howMuch) {
+        this.speed += howMuch;
+    }
+
+    public void decreaseSpeed(int howMuch) {
+        //this.speed -= howMuch;
+        setSpeed(this.speed - howMuch);
+    }
 }
+
+public class Encapsulation {
+    public static void main(String[] args) {
+
+        encapsulate bike = new encapsulate(10);
+        System.out.println(bike.getSpeed());
+
+        bike.increaseSpeed(10);
+        System.out.println(bike.getSpeed());
+        bike.decreaseSpeed(100);
+        System.out.println(bike.getSpeed());
+    }
+
+}
+
+//Data Hiding
+//Increased Flexibility
+//Reusability
