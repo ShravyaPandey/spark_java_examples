f643644ea02c2d2e9158e96620782221100de1ce
Shravya <shravyapandey0@gmail.com>
2024-08-30 00:59:24 +0530

    seventh_commit

--- src/main/java/ParkingLot/ParkingSystem.java
+++ src/main/java/ParkingLot/ParkingSystem.java
@@ -1,2 +1,43 @@
-package ParkingLot;public class ParkingSystem {
+package ParkingLot;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ParkingSystem {
+
+    private String ParkingLotId;
+    Map<String, ParkingFloor> parkingLot;
+
+    public ParkingSystem(String ParkingLotId) {
+        this.ParkingLotId = ParkingLotId;
+        parkingLot = new HashMap<>();
+    }
+
+    public void addFloor(ParkingFloor floor) {
+        parkingLot.put(floor.getFloorId(), floor);
+    }
+
+    public ParkingFloor getFloor(String floorId) {
+        return parkingLot.get(floorId);
+    }
+
+    public int getFreeCount() {
+        int totalFree = 0;
+        for (ParkingFloor floor : parkingLot.values()) {
+            totalFree += floor.getFreeCount();
+        }
+        return totalFree;
+    }
+
+    public int getOccupiedCount() {
+        int occupiedCount = 0;
+        for (ParkingFloor floor : parkingLot.values()) {
+            occupiedCount += floor.getOccupiedCount();
+        }
+        return occupiedCount;
+    }
+
 }
--- src/main/java/ParkingLot/driverClass.java
+++ src/main/java/ParkingLot/driverClass.java
@@ -1,2 +1,55 @@
-package ParkingLot;public class driverClass {
+package ParkingLot;
+
+public class driverClass {
+
+    public static void main(String[] args) {
+        ParkingSystem parkingLot = new ParkingSystem("PR1234");
+        ParkingFloor floor1 = new ParkingFloor("Floor1");
+        ParkingFloor floor2 = new ParkingFloor("Floor2");
+        ParkingLane lane1 = new ParkingLane(1);
+        ParkingLane lane2 = new ParkingLane(2);
+        int numberOfSpotsPerLane = 3;
+        for (int i = 1; i <= numberOfSpotsPerLane; i++) {
+            lane1.addSpot(i, VehicleType.TRUCK);
+        }
+
+        numberOfSpotsPerLane = 4;
+        for (int i = 1; i <= numberOfSpotsPerLane; i++) {
+            lane2.addSpot(i, null);
+        }
+        floor1.addLane(lane1);
+        floor2.addLane(lane2);
+        parkingLot.addFloor(floor1);
+        parkingLot.addFloor(floor2);
+
+
+        Vehicle vehicle1 = new Car(VehicleType.CAR, "1234", "red");
+
+        Vehicle vehicle2 = new Truck(VehicleType.TRUCK, "1235", "red");
+        Vehicle vehicle3 = new Bike(VehicleType.BIKE, "1235", "red");
+
+        //parking and ticketing system
+
+
+        System.out.println("Total Free Slots: " + parkingLot.getFreeCount());
+        System.out.println("Total Occupied Slots: " + parkingLot.getOccupiedCount());
+        System.out.println("Free Slots in Floor1: " + parkingLot.getFloor("Floor1").getFreeCount());
+        System.out.println("Occupied Slots in Floor1: " + parkingLot.getFloor("Floor1").getOccupiedCount());
+        System.out.println("Free Slots in Floor2: " + parkingLot.getFloor("Floor2").getFreeCount());
+        System.out.println("Occupied Slots in Floor2: " + parkingLot.getFloor("Floor2").getOccupiedCount());
+        System.out.println("Free Slots in Floor2, Lane2: " + parkingLot.getFloor("Floor2").getLane(2).getFreeCount());
+        System.out.println("Occupied Slots in Floor1, Lane1: " + parkingLot.getFloor("Floor1").getLane(1).getOccupiedCount());
+
+        parkingLot.getFloor("Floor2").getLane(2).getSpot(1).parkVehicle(vehicle1);
+        System.out.println("Total Free Slots: " + parkingLot.getFreeCount());
+        System.out.println("Total Occupied Slots: " + parkingLot.getOccupiedCount());
+        System.out.println("Free Slots in Floor1: " + parkingLot.getFloor("Floor1").getFreeCount());
+        System.out.println("Occupied Slots in Floor1: " + parkingLot.getFloor("Floor1").getOccupiedCount());
+        System.out.println("Free Slots in Floor2: " + parkingLot.getFloor("Floor2").getFreeCount());
+        System.out.println("Occupied Slots in Floor2: " + parkingLot.getFloor("Floor2").getOccupiedCount());
+        System.out.println("Free Slots in Floor2, Lane2: " + parkingLot.getFloor("Floor2").getLane(2).getFreeCount());
+        System.out.println("Occupied Slots in Floor1, Lane1: " + parkingLot.getFloor("Floor1").getLane(1).getOccupiedCount());
+
+
+    }
 }
